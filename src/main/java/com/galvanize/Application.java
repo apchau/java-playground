de6feaf101a050fee75ff6d4c30f8016450c6b17
Apoorv Chaudhary <apoorv.c2@gmail.com>
2018-02-15 19:10:14 -0800

    initial commit

---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,11 @@
+package com.galvanize;
+
+/**
+ * Created by apoorv on 2/15/18.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
